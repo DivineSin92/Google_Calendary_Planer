48463725951d5f1e8057da8a7e7efabe2665f0aa
Michał Mazur <147649867+DivineSin92@users.noreply.github.com>
2023-10-18 20:14:34 +0200

    Update planer.py

--- planer.py
+++ planer.py
@@ -11,7 +11,6 @@ class txtcolors:
     normal = '\033[0m'
     No_events = '\33[91m'
 
-
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def main():
@@ -26,31 +25,37 @@ def main():
             creds = flow.run_local_server(port=0)
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
+    print(f'{chr(27) + "[2J"}')
     print('(1) Show events for selected day')
     print('(2) Show closest upcoming x events')
     print('(3) Add new upcoming event')
     print('(4) Edit event')
-    print('(5) Search for event by name') #w zdarzenia list "q"
+    print('(5) Search for event by name\n') #w zdarzenia list "q"
     program = int(input('Select program: '))
 
     if program == 1:
         input_date = input('Please input date in format YYYY-MM-DD: ')
+        date_format = '%Y-%m-%d'
         try:
-            service = build('calendar', 'v3', credentials=creds)
-            print(f'Getting the upcoming x events')
-            events_result = service.events().list(calendarId='primary', timeMin=input_date + 'T00:00:00-00:00', timeMax=input_date + 'T23:59:59-00:00', maxResults=10, singleEvents=True, orderBy='startTime').execute()
-            events = events_result.get('items', [])
-            if not events:
-                print(f'{txtcolors.No_events}You dont have upcoming events!{txtcolors.normal}')
-                return
-            for event in events:
-                start = event['start'].get('dateTime', event['start'].get('date'))
-                print(start, event['summary'])
-        except HttpError as error:
-            print('Error: %s' % error)
+            date_check = datetime.datetime.strptime(input_date, date_format)
+            try:
+                service = build('calendar', 'v3', credentials=creds)
+                print(f'Getting the upcoming events for this day')
+                events_result = service.events().list(calendarId='primary', timeMin=input_date + 'T00:00:00-00:00', timeMax=input_date + 'T23:59:59-00:00', singleEvents=True, orderBy='startTime').execute()
+                events = events_result.get('items', [])
+                if not events:
+                    print(f'{txtcolors.No_events}You dont have upcoming events!{txtcolors.normal}')
+                    return
+                for event in events:
+                    start = event['start'].get('dateTime', event['start'].get('date'))
+                    print(start, event['summary'])
+            except HttpError as error:
+                print('Error: %s' % error)
+        except ValueError:
+            print('Incorect format of date')
 
     if program == 2:
-        numb_of_events = int(input('Select number of showing events: '))
+        numb_of_events = int(input('Select number of showing event(s): '))
         try:
             service = build('calendar', 'v3', credentials=creds)
             now = datetime.datetime.utcnow().isoformat() + 'Z'
